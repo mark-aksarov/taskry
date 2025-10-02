374d563b4841a0eb34b864b7eed1df9dba3b5e76
markak <aksarov91@gmail.com>
2025-10-02 17:07:13 +0500

    Change font size in ProfileLink

--- components/layout/ProfileLink/ProfileLink.tsx
+++ components/layout/ProfileLink/ProfileLink.tsx
@@ -14,7 +14,7 @@ export const ProfileLink = () => {
       <div className="h-8 w-8 overflow-hidden rounded-full bg-gray-300">
         <Image src="/man.jpg" alt="user avatar" width={32} height={32} />
       </div>
-      <span className="font-bold text-black max-md:text-sm md:text-lg dark:text-white">
+      <span className="font-bold text-black max-md:text-sm md:text-base dark:text-white">
         Adam Smith
       </span>
     </Link>
