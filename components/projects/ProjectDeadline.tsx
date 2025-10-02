f2f8c97236b05463a42a9330331c6ce3ee1479cc
markak <aksarov91@gmail.com>
2025-10-02 09:40:28 +0500

    Fix useResponsiveOverlayType hook and ProjectDeadline component

--- components/projects/ProjectDeadline.tsx
+++ components/projects/ProjectDeadline.tsx
@@ -3,8 +3,14 @@ import { ResponsiveDatePicker } from "../common/ResponsiveDatePicker";
 export function ProjectDeadline() {
   return (
     <div className="flex flex-col gap-4">
-      <ResponsiveDatePicker label="Deadline from" />
-      <ResponsiveDatePicker label="Deadline to" />
+      <ResponsiveDatePicker
+        overlayClassName="w-[var(--trigger-width)]"
+        label="Deadline from"
+      />
+      <ResponsiveDatePicker
+        overlayClassName="w-[var(--trigger-width)]"
+        label="Deadline to"
+      />
     </div>
   );
 }
--- lib/hooks/useResponsiveOverlayType.ts
+++ lib/hooks/useResponsiveOverlayType.ts
@@ -1,7 +1,7 @@
 import { useMediaQuery } from "react-responsive";
 
 export const useResponsiveOverlayType = () => {
-  const isMd = useMediaQuery({ query: "(max-width: 28rem)" });
+  const isMd = useMediaQuery({ query: "(max-width: 48rem)" });
 
   return isMd ? "bottomsheet" : "popover";
 };
