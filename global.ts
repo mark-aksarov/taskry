545265dc05b70c756fee41854b9a14cad1d2ca5b
markak <aksarov91@gmail.com>
2026-01-26 10:12:47 +0500

    Fix i18n messages

--- global.ts
+++ global.ts
@@ -1,16 +1,9 @@
 import { routing } from "@/i18n/routing";
 import messagesEn from "./messages/en";
-import messagesRu from "./messages/ru";
-
-type CommonKeys<A, B> = {
-  [K in keyof A & keyof B]: A[K] & B[K];
-};
-
-type Messages = CommonKeys<typeof messagesEn, typeof messagesRu>;
 
 declare module "next-intl" {
   interface AppConfig {
     Locale: (typeof routing.locales)[number];
-    Messages: Messages;
+    Messages: typeof messagesEn;
   }
 }
