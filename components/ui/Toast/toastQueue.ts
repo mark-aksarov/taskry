f3ae6fcad0aafe98788b541bf4185e1898e4442b
markak <aksarov91@gmail.com>
2026-03-02 10:15:24 +0500

    Implement toast animation

--- components/ui/Toast/toastQueue.ts
+++ components/ui/Toast/toastQueue.ts
@@ -1,10 +1,18 @@
 "use client";
 
+import { flushSync } from "react-dom";
 import { ToastContent } from "./Toast";
 import { UNSTABLE_ToastQueue as RACToastQueue } from "react-aria-components";
 
 export const toastQueue = new RACToastQueue<ToastContent>({
+  // Wrap state updates in a CSS view transition.
   wrapUpdate(fn) {
-    fn();
+    if ("startViewTransition" in document) {
+      document.startViewTransition(() => {
+        flushSync(fn);
+      });
+    } else {
+      fn();
+    }
   },
 });
