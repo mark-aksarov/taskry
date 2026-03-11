da9e37496ff3b656a445ab7f5520ae343464c786
markak <aksarov91@gmail.com>
2026-03-11 11:48:13 +0500

    Adjust dashboard cards grid breakpoints

--- components/common/DashboardCardsGrid.tsx
+++ components/common/DashboardCardsGrid.tsx
@@ -6,7 +6,7 @@ export function DashboardCardsGrid({
   return (
     <div
       data-test="dashboard-cards"
-      className="grid max-md:grid-cols-1 max-md:gap-4 md:gap-6 md:max-xl:grid-cols-2 xl:grid-cols-4"
+      className="grid max-md:grid-cols-1 max-md:gap-4 md:gap-6 md:max-lg:grid-cols-2 lg:grid-cols-4"
     >
       {children}
     </div>
