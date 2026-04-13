bf6716c36c6e5fd659861e8d081dd88ec9ac0c3a
markak <aksarov91@gmail.com>
2026-04-13 21:08:18 +0500

    Fix project status filters not being shown

--- components/projects/ProjectStatusFiltersModal/ProjectStatusFiltersModal.tsx
+++ components/projects/ProjectStatusFiltersModal/ProjectStatusFiltersModal.tsx
@@ -9,6 +9,7 @@ import { useTranslations } from "next-intl";
 import { useProjectFilters } from "../ProjectFiltersContext";
 import { DialogBody, DialogFooter } from "@/components/ui/Dialog";
 import { FormBaseModal } from "@/components/common/FormBaseModal";
+import { useModal } from "@/components/common/ModalManagerContext";
 import { ProjectStatusFiltersForm } from "../ProjectStatusFiltersForm";
 import { FilterModalDialog } from "@/components/common/FilterModalDialog";
 import { FiltersFormSubmitButton } from "@/components/common/FiltersForm";
@@ -16,9 +17,14 @@ import { FilterModalDialogHeader } from "@/components/common/FilterModalDialogHe
 
 export function ProjectStatusFiltersModal() {
   const initialFilters = useProjectFilters();
+  const { isOpen, onOpenChange } = useModal("projectStatusFilters");
 
   return (
-    <FormBaseModal data-test="project-status-filters-modal">
+    <FormBaseModal
+      data-test="project-status-filters-modal"
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+    >
       <ProjectFiltersFormProvider initialFilters={initialFilters}>
         <FilterModalDialog>
           <DialogHeader />
