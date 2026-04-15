729ec49fcf629fb78acb889ce8047756c5db45a4
markak <aksarov91@gmail.com>
2026-04-16 00:46:07 +0500

    Fix title in ProjectDetailCard and TaskDetailCard

--- components/projects/ProjectDetailCard/ProjectDetailCard.tsx
+++ components/projects/ProjectDetailCard/ProjectDetailCard.tsx
@@ -1,8 +1,9 @@
-import { Card } from "@/components/common/Card";
 import {
   DetailCardHeader,
   DetailCardTitle,
 } from "@/components/common/DetailCard";
+import { useTranslations } from "next-intl";
+import { Card } from "@/components/common/Card";
 
 interface ProjectDetailCardProps {
   projectDetailContainer: React.ReactNode;
@@ -11,10 +12,12 @@ interface ProjectDetailCardProps {
 export function ProjectDetailCard({
   projectDetailContainer,
 }: ProjectDetailCardProps) {
+  const t = useTranslations("projects.ProjectDetailCard");
+
   return (
     <Card className="mx-auto w-[700px] max-w-full p-0">
       <DetailCardHeader className="max-md:hidden">
-        <DetailCardTitle>Детали проекта</DetailCardTitle>
+        <DetailCardTitle>{t("title")}</DetailCardTitle>
       </DetailCardHeader>
       <div className="max-md:p-4 md:p-6">{projectDetailContainer}</div>
     </Card>
--- components/tasks/TaskDetailCard/TaskDetailCard.tsx
+++ components/tasks/TaskDetailCard/TaskDetailCard.tsx
@@ -1,18 +1,21 @@
-import { Card } from "@/components/common/Card";
 import {
   DetailCardHeader,
   DetailCardTitle,
 } from "@/components/common/DetailCard";
+import { useTranslations } from "next-intl";
+import { Card } from "@/components/common/Card";
 
 interface TaskDetailCardProps {
   taskDetailContainer: React.ReactNode;
 }
 
 export function TaskDetailCard({ taskDetailContainer }: TaskDetailCardProps) {
+  const t = useTranslations("tasks.TaskDetailCard");
+
   return (
     <Card className="mx-auto w-[700px] max-w-full p-0">
       <DetailCardHeader className="max-md:hidden">
-        <DetailCardTitle>Детали задачи</DetailCardTitle>
+        <DetailCardTitle>{t("title")}</DetailCardTitle>
       </DetailCardHeader>
       <div className="max-md:p-4 md:p-6">{taskDetailContainer}</div>
     </Card>
