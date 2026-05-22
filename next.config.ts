75a2590d994d121d7c5bc094074ada8f74347c0f
markak <aksarov91@gmail.com>
2026-05-22 16:37:08 +0500

    Refactor S3 endpoint handling in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,24 +2,23 @@ import { NextConfig } from "next";
 import createMDX from "@next/mdx";
 import createNextIntlPlugin from "next-intl/plugin";
 
-if (!process.env.S3_ENDPOINT) {
-  throw new Error("S3_ENDPOINT is not defined");
-}
-
-const endpointUrl = new URL(process.env.S3_ENDPOINT);
+const s3Endpoint = process.env.S3_ENDPOINT;
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   images: {
     contentDispositionType: "inline",
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: endpointUrl.hostname,
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    remotePatterns: s3Endpoint
+      ? [
+          {
+            protocol: "https",
+            hostname: new URL(s3Endpoint).hostname,
+            port: "",
+            pathname: "/**",
+          },
+        ]
+      : [],
   },
   experimental: {
     mdxRs: true,
@@ -31,4 +30,5 @@ const withMDX = createMDX({
 });
 
 const withNextIntl = createNextIntlPlugin();
+
 export default withNextIntl(withMDX(nextConfig));
