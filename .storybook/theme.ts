cc13fdde03da2f624d65fcf560ccf57a27fc42b0
markak <aksarov91@gmail.com>
2026-05-29 15:36:04 +0500

    Update storybook theming

---
+++ .storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from "storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: `<div style="font-size: 20px; color: #000; font-weight: 600">Taskry</div>`,
+  brandUrl: "https://taskry.ru",
+  brandTarget: "_self",
+});
