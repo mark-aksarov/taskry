2125b7c9ad6ff11975f1929d02ab67d2c742512b
markak <aksarov91@gmail.com>
2026-07-26 17:12:10 +0500

    Adjust GitHub icon size

--- icons/Github.tsx
+++ icons/Github.tsx
@@ -1,8 +1,8 @@
 export function Github() {
   return (
     <svg
-      width="24"
-      height="24"
+      width="20"
+      height="20"
       viewBox="0 0 98 96"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
